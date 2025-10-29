4ab5349bd68116be94dfb14df751a5e6b7cd78cc
Sara Candelario Peña <scandelariop@uoc.edu>
2025-10-29 15:08:31 +0000

    Dependency Animation.css
    - Installation and configuration of Animation.css
    - Application of new dependency in index.html
    - Creation of function in main.js

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,6 +10,9 @@
  * Write any other JavaScript below
  */
 
+// -----------------------------
+// Color del botón contacto adaptado según el color del fondo
+// -----------------------------
 document.addEventListener("DOMContentLoaded", () => { //DOMContentLoaded para q se ejecute cuando todo el html esté cargado
   const btnContacto = document.querySelector(".contacto");
 
@@ -20,4 +23,28 @@ document.addEventListener("DOMContentLoaded", () => { //DOMContentLoaded para q 
       console.log("Botón de contacto pulsado \uD83D\uDC4D"); //+ mensajito para verificar que se ha clicado
     });
   }
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".places__grid"); //selecciona el 1r elem. places__grid
+  const title = document.querySelector(".places__second-title");
+
+  grid.classList.add("animate__animated", "animate__slower"); //añado clase animate__animated al inicio y slower para q sea + lenta
+
+  const observer = new IntersectionObserver( //para detectar cuando es visible en pantalla
+    (entries, obs) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) { //en caso de que sea true:
+          grid.classList.add("animate__fadeInUp"); //se agrega la animación al grid
+          obs.unobserve(entry.target); //sólo una vez!!
+        }
+      });
+    },
+    {
+      threshold: 0.5, //se dispara cuando esté al 50% visible
+    }
+  );
+
+  observer.observe(title);
 });
